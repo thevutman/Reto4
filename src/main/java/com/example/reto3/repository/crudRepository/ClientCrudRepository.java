e0c081fb6116e4480e6f2f155c9cc4b73ed20bfd
Santiago Serrano <santiagosg0706@gmail.com>
2022-10-11 16:54:57 -0500

    se agregó el crudrepository de client

---
+++ src/main/java/com/example/reto3/repository/crudRepository/ClientCrudRepository.java
@@ -0,0 +1,7 @@
+package com.example.reto3.repository.crudRepository;
+
+import com.example.reto3.entities.Client;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClientCrudRepository extends CrudRepository<Client,Integer> {
+}
