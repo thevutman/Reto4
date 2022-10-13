9c3e250bae939c583046e0c366664ce978bba28c
Santiago Velasco <santiagovelascomosquera@hotmail.com>
2022-10-12 23:50:20 -0500

    Cambio apllicationpropities x8

--- src/main/java/com/example/reto3/controller/AdminController.java
+++ src/main/java/com/example/reto3/controller/AdminController.java
@@ -3,10 +3,12 @@ package com.example.reto3.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.reto3.entities.Admin;
@@ -24,6 +26,7 @@ public class AdminController {
     }
 
     @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
     public Admin save(@RequestBody Admin p){
         return adminService.save(p);
     }
